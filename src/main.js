bd5d904505298d65ed4af237321cee479b7e2e54
AlexandreTomasi <alexandretomasi18@gmail.com>
2021-01-27 18:03:39 -0400

    Configurando o main.js para carregar os componentes padrao de configuração. Bootstrap.js para sempre carregar as opções do bootstrap. mq.js para configurações de responsividade. store.js o qual será usado para salvar dados para toda a aplicação.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 import App from './App.vue'
 
+import './config/bootstrap'
+import './config/msgs'
+import './config/mq'
+import store from './config/store'
 Vue.config.productionTip = false
 
+
 new Vue({
+  store,
+  //router,
   render: h => h(App),
 }).$mount('#app')
