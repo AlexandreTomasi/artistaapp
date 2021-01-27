56414245f1afe728c5f486e11ee362bf54c0e83a
AlexandreTomasi <alexandretomasi18@gmail.com>
2021-01-27 18:37:03 -0400

    Criação dos templates principais da tela do sistema são eles: Header(cabeçalho), Content(conteúdo), Footer(rodapé). Criado a grid de area, ou seja a definição dos espaços da tela do sistema na classe raiz principal do sistema App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
+		<Header title="Polícia Judiciária Civil - Sistema de Álbuns de artistas" 
+			:hideToggle="!user"
+			:hideUserDropdown="!user" />
+		<Menu v-if="user" />
+		<Loading v-if="validatingToken" />
+		<Content v-else />
+		<Footer />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState} from 'vuex'
+import Header from "@/components/template/Header"
+import Menu from "@/components/template/Menu"
+import Content from "@/components/template/Content"
+import Footer from "@/components/template/Footer"
+import Loading from "@/components/template/Loading"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: {Header, Menu, Content, Footer, Loading},
+  computed: mapState(['isMenuVisible', 'user']),
+  data: function() {
+		return {
+			validatingToken: true
+		}
+	},
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: "Lato", sans-serif;
+  }
+  body{
+    margin: 0;
+  }
+
+  #app{
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    height: 100vh;
+    display: grid;
+    grid-template-rows: 80px 1fr 40px;
+    grid-template-columns: 200px 1fr;
+    grid-template-areas: 
+    "header header"
+    "menu content"
+    "menu footer"
+  }
+
+  #app.hide-menu{
+    grid-template-areas:
+    "header header"
+    "content content"
+    "footer footer"
+  }
+  
 </style>
